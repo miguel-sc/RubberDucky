07d6a98ee9847f9f2a609ab06916981fa0219d81
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2018-02-07 16:49:36 +0100

    added webgl support check and Box2D notice

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import './favicon.ico'
 import './lib/liquidfun/liquidfun.min.js'
 import App from './App'
 
-document.body.appendChild(App.view)
-App.init()
+function webgl_support() {
+  try{
+    var canvas = document.createElement('canvas')
+    return !! window.WebGLRenderingContext && (
+      canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
+  }catch(e) { return false }
+}
+
+if(webgl_support()) {
+  document.body.appendChild(App.view)
+  App.init()
+}
